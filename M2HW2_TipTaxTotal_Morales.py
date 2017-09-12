5f426a2e789e4d2bdebda71dd7151621641d3d9d
Moralesc1427 <31833665+Moralesc1427@users.noreply.github.com>
2017-09-11 21:44:23 -0400

    Add files via upload

---
+++ M2HW2_TipTaxTotal_Morales.py
@@ -0,0 +1,16 @@
+# CTI - 110
+# M2HW2 - Tip Tax Total
+# Carlos Morales
+# 09/11/2017
+foodcost = float (input('Enter food cost: '))
+salestax = .07
+tipamount = .18
+subtotal = foodcost
+tax = subtotal * salestax
+totalcost = subtotal + tax
+tip = subtotal * tipamount
+print("food cost",foodcost)
+print("subtotal",foodcost)
+print("tax",tax)
+print("totalcost",totalcost)
+print("tip",tip)
